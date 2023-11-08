49647c56c93491a0059ad0a7995b944990e4159f
t2del <macapagal.dennis@gmail.com>
2023-11-08 14:06:51 +0800

    menu for group page

---
+++ src/config/nav.js
@@ -0,0 +1,38 @@
+export const navGroup = [
+	{
+	  title: 'Home',
+	  slug: '/fhg/',
+	},
+	// {
+	//   title: 'About',
+	//   slug: '/t2delastro/about',
+	// },
+	{
+	    title: 'Blog',
+	    slug: '/fhg/blog',
+	    submenu: [
+            {
+                title: 'First post',
+                slug: '/fhg/blog/first-post',
+            },
+            {
+                title: 'Second post',
+                slug: '/fhg/blog/second-post',
+            },
+        ]
+	// 	{
+	// 		title: 'Third post',
+	// 		slug: '/t2delastro/blog/third-post',
+	// 	},
+	//   ],
+	},
+	// {
+	//   title: 'Portfolio',
+	//   slug: '/t2delastro/portfolio',
+	// },
+	{
+		title: 'Contact',
+		slug: '/fhg/contact-us',
+	  },
+  ];
+
