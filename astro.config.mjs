007f42ec0a94af2abc59a63b34ceedfec53c99ab
t2del <macapagal.dennis@gmail.com>
2023-11-14 19:58:53 +0800

    minor edit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import partytown from "@astrojs/partytown";
 // import nodejs from '@astrojs/node';
 
 // https://astro.build/config
@@ -8,12 +7,4 @@ export default defineConfig({
     base: '/fhg/',
     // output: 'server',
     // adapter: nodejs(),
-    integrations: [
-        partytown({
-          // Adds dataLayer.push as a forwarding-event.
-          config: {
-            forward: ["dataLayer.push"],
-          },
-        }),
-      ],
 });
